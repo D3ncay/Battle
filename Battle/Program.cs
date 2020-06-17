0ab75d003b9d0784ace8f012a49cfa32e14ed6ca
D3ncay <dencay@mail.ru>
2020-06-17 22:57:39 +0300

    Добавьте файлы проекта.

---
+++ Battle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Warrior
+    {
+        public int Health { get; private set; }
+        public int Armor { get; private set; }
+        public int Damage { get; private set; }
+        public Warrior(int health, int armor, int damage)
+        {
+            Health = health;
+            Armor = armor;
+            Damage = damage;
+        }
+        public void TakeDamage()
+        {
+            Health -= Damage + Armor;
+        }
+    }
+
+    class NightElf : Warrior ()
+    {
+
+    }
+
+}
